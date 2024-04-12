1cfc536862130452ba227c0a0c7f9872aad81aae
doingkite <155817735+doingkite@users.noreply.github.com>
2024-04-12 15:03:21 +0900

    mode
    start, modesetting1, 2, 3

---
+++ Weird Pocket ball/Assets/Scripts/ModeButton2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//SceneManagement
+using UnityEngine.SceneManagement;
+
+public class ModeButton2 : MonoBehaviour 
+{
+    public void Button2()
+    {
+        SceneManager.LoadScene("ModeSetting3"); 
+    }
+}
+
