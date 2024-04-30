e62324b5ca82925b6d5fb1b1540869c893f0e1c6
Meongjeong <258xsw@naver.com>
2024-04-30 16:35:57 +0900

    빙글빙글 돌아가는 공

--- Weird Pocket ball/Assets/Script/ScreenTouchManager.cs
+++ Weird Pocket ball/Assets/Script/ScreenTouchManager.cs
@@ -1,44 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ScreenTouchManager : MonoBehaviour
 {
-    [SerializeField] private BallSystem ballSystem;
-   // [SerializeField] private CueController cueController;
-
+    public GameObject ball;
     public GameObject cue;
-    public Vector3 mousePosition;
-    // Start is called before the first frame update
-    private CueController cueController;
+    public Vector3 prevPos;
+    LineRenderer cueLineRenderer;
     private void Start()
     {
-        cueController = cue.GetComponent<CueController>();    
+        cueLineRenderer = ball.GetComponent<LineRenderer>();
+        cueLineRenderer.startWidth = 0.05f; // 선의 시작 너비
+        cueLineRenderer.endWidth = 0.05f; // 선의 끝 너비
     }
+
     private void OnMouseDown()
     {
-        //라인렌더러 출력
-           Debug.Log("OnMouseDown");
-        //   //ShootCue(Input.mousePosition);
-      //  ballSystem.OnLineRenderer();
+        prevPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        cueLineRenderer.GetComponent<LineRenderer>().enabled = true;
+        cue.SetActive(true);
+       
     }
     private void OnMouseUp()
     {
-        //라인렌더러 제거 및 발사
         Debug.Log("OnMouseUp");
-        //ballSystem.AddForce();
-    //    cueController.AddForce();
+        cueLineRenderer.GetComponent<LineRenderer>().enabled = false;
+        cue.SetActive(false);
        
     }
     private void OnMouseDrag()
     {
-        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10); // 마우스 위치
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-        worldPos.z = 0; // z 좌표는 0으로 설정해 2D 플레인에 맞춤
-        cueController.DrowLineRenderer(mousePos);
+        
 
-        Vector2 direction = (worldPos - cue.transform.position).normalized; // 당구대에서 마우스 위치로의 방향 벡터
+
+            //cueController.DrowLineRenderer(mousePos);
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10); // 마우스 위치
+        Vector2 direction = (prevPos - mousePos).normalized;
+        Vector3 lineRednderPos = new Vector3(direction.x, ball.transform.position.y / 5, direction.y);
+
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
+        Physics.Raycast(transform.position, transform.forward, out hit);
+
+        cueLineRenderer.SetPosition(0, ball.transform.position);
+        cueLineRenderer.SetPosition(1, lineRednderPos * hit.distance);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90; // -90도 조정
-        cue.transform.rotation = Quaternion.Euler(0, angle, 0); // 회전 적용
+        ball.transform.rotation = Quaternion.Euler(0, -angle, 0); // 회전 적용
     }
 }
