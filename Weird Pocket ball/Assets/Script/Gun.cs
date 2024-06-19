cf58b255cffce610c000a750af0d52c8f1800fb8
Meongjeong <258xsw@naver.com>
2024-06-20 04:16:10 +0900

    메인 기능 최종 수정

--- Weird Pocket ball/Assets/Script/Gun.cs
+++ Weird Pocket ball/Assets/Script/Gun.cs
@@ -38,26 +38,30 @@ public class Gun : MonoBehaviour
     
     public void Shoot()
     {
-        // 총알 생성
-        GameObject bullet = Instantiate(bulletPrefab, shootingPointStart.position, shootingPointStart.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (ScreenTouchManager.isTouch)
+        {
+            // 총알 생성
+            GameObject bullet = Instantiate(bulletPrefab, shootingPointStart.position, shootingPointStart.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
 
-        // 발사 방향 계산: shootingPointStart에서 shootingPointEnd까지의 벡터의 반대 방향
-        Vector3 direction = (shootingPointStart.position - shootingPointEnd.position).normalized;
-        this.gameObject.SetActive(false);
+            // 발사 방향 계산: shootingPointStart에서 shootingPointEnd까지의 벡터의 반대 방향
+            Vector3 direction = (shootingPointStart.position - shootingPointEnd.position).normalized;
+            this.gameObject.SetActive(false);
 
-        Debug.Log(this.gameObject);
-        ball.GetComponent<LineRenderer>().enabled = false;
-        this.gameObject.SetActive(false);
-        // 총알에 힘을 가해 발사
-        rb.velocity = direction * bulletSpeed;
+            Debug.Log(this.gameObject);
+            ball.GetComponent<LineRenderer>().enabled = false;
+            this.gameObject.SetActive(false);
+            // 총알에 힘을 가해 발사
+            rb.velocity = direction * bulletSpeed;
 
-        // 일정 시간 후 총알을 파괴 (옵션)
-        // Destroy(bullet, 2.0f);  // 2초 후 총알 파괴
+            // 일정 시간 후 총알을 파괴 (옵션)
+            // Destroy(bullet, 2.0f);  // 2초 후 총알 파괴
 
-        //isStart = true;
+            //isStart = true;
+            ScreenTouchManager.isTouch = false;
+            GameManager.turn = !GameManager.turn;
+            gameManager.BallMovementStatus();
+        }
         
-        GameManager.turn = !GameManager.turn;
-        gameManager.BallMovementStatus();
     }
 }
--- Weird Pocket ball/Assets/Scripts/ModeSelectController.cs
+++ Weird Pocket ball/Assets/Scripts/ModeSelectController.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Palmmedia.ReportGenerator.Core.Parser.Analysis;
-using Unity.VisualScripting;
+
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 
 public class ModeSelectController : MonoBehaviour
